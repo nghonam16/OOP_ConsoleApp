9ce69d47d7b6d43e7fc7db6abf4bee1eac5074dc
Nguyen Nam <nghoofnam@gmail.com>
2025-05-28 23:23:22 +0700

    Add Utils file

---
+++ lib/Utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <vector>
+#include <string>
+
+bool isPrime(int number);
+void print(const std::string& prompt, const std::vector<int>& numbers);
+
+#endif // UTILS_H
